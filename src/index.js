3ca0057c4a16b8444a599b9df08b73e65b8beb81
Marcelo Bueno <marcelobueno25@hotmail.com>
2019-08-05 21:44:58 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar
+} from "react-native";
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions
+} from "react-native/Libraries/NewAppScreen";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>teste</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff"
+  }
+});
+
+export default App;
